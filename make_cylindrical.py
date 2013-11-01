80c9c6cfba0af348554d99e716cb85ebd96065fd
suavemint <ej.orcutt@gmail.com>
2013-11-01 18:58:53 -0500

    Testing local addition...

--- make_cylindrical.py
+++ make_cylindrical.py
@@ -1,5 +1,38 @@
 import numpy as np
 
+# ASSUMPTION: Inputted elastic constants for BZO and YBCO are assumed to be in dict format,
+# + keyed by {...,(i,j): #.###,...} !
+def elastic_energy(c1, c2, R, D):
+    """
+    Calcuates the elastic energy, for cylindrical system of BZO NRs in a YBCO film;
+    NB: Need to move many of the calculations for constants in this function to a
+    more universal namespace; for every rho at which the total elastic energy
+    density is calculated requires recomputation of constants.
+    """
+
+    import numpy as np
+
+    rho = R*R / D*D  # Volume density of BZO. ** For further calculations, assume rho << 1. **
+
+    lambda_1 = (c_1[(1,1)] + c_1[(1,2)]) / c_1[(1,3)]
+    lambda_2 = (c_2[(1,1)] + c_2[(1,2)]) / c_2[(1,3)]
+
+    # TODO: Does f_3 == f_z?? #####
+    f_3 = (C2 - C1) / C1
+    f_theta = np.sqrt(2)*f_x
+
+    a_0 = (f_3(1+f_theta)+lambda_2*f_theta*(1+f3))/(c_1[(1,3)]*(lambda_1*(1+f_theta) - lambda_2*(1+f_3)))
+    # b_0 =   # DNE
+
+    a_1 = a_0 / (np.ln(D) - np.ln(R))
+    b_1 = ( f3 + lambda_1*f_theta ) / (c_2[(1,3)] * ( lambda_1*(1+f_theta)-lambda_2*(1+f_3) ))
+
+
+    v_1 = 0.5 * ( c1[(1,1)] + c1[(1,2)] ) - ( 1/c1[(3,3)] ) * ( c1[(1,3)] * c1[(1,3)] )
+    v_2 = 0.5 * ( c2[(1,2)] + c2[(1,2)] ) - ( 1/c2[(3,3)] ) * ( c2[(1,3)] * c2[(1,3)] )
+
+    E = (c1[(1,1)] + c1[(1,2)])*c[(3,3)]*v_1*((2*(1-rho)+rho*np.ln(rho*(2-np.ln(rho))))/(np.ln(rho)*np.ln(rho)))*a_0*a_0 + (c_2[(1,1)]+c_2[(1,2)])*c_2[(3,3)]*v_2*b_1*b_1*rho
+
 # Going to test just converting a simple Cartesian mesh to a cylindrical one.
 # coordinates: (x,y,z) --> (r,phi,z)
 # conversion: r = sqrt(x^2+y^2+z^2); phi = arctan(y/x); z = z
@@ -39,6 +72,14 @@ def strain_equation():
     # The average elastic density for a YBCO/BZO cylindrical system is
     # E = \frac{1}{\piD^2}\left[\piR^2E_{BZO} + \pi(D^2 - R^2)E_{YBCO}\right]
     #   = 0.5*(c11^1+c12^1)*[c33^1*(c11^1+c12^1) - 2*c13^1*c13^1]*\frac{2(1-\rho)+\rho*log(\rho)*(2-log(\rho))}{(log(\rho))^2}*A_0^2
+
+def solve():
+    # First, let's define some constants to use in the equations:
+    a1 = e1/(c11 + c12)  # TODO: Need to add in an ability to choose materials' constants here.
+    # In eq. above, what is e1?
+
+    b1 = e2/(c11 + c12)
+    b2 = c/(c11 + c12)
 
 if __name__=='__main__':
     run_it()
--- testing_domains.py
+++ testing_domains.py
@@ -1,5 +1,126 @@
 import dolfin as d
 import numpy as np
 
-class test:
+def cart_to_cyl(in_tuple):
+    x, y, z = in_tuple
+    r = np.sqrt(x*x + y*y + z*z)
+    if x != 0.:
+        phi = np.arctan2(y/x)
+    else:
+        phi = 0.
+    return r, phi, z
 
+#set_log_level(PROGRESS)
+# The first test of the package and my abilities to work with PDE solvers
+# + will be with the Poisson solver:
+
+# Using Fenics:
+# 1. Identify the PDE and its BCs
+# 2. Reformulate the PDE problem as a variational one
+# 3. Make script where formulas in the variational problem
+# +  are coded, along with input data such as f, u0, and a mesh for \Omega.
+# 4. Add statements in the script for solving the variational problem,
+# +  computing derived quantities such as grad(u), or visualizing.
+
+#print 'Running simple test of Poisson-solving...'
+#
+#mesh = d.UnitSquareMesh(32, 32)
+#V = d.FunctionSpace(mesh, "Lagrange", 1)
+#u = d.TrialFunction(V)
+#v = d.TestFunction(V)
+#
+#f = d.Expression('x[0]*x[1]')
+#
+#dx = d.dx
+#a = d.dot(d.grad(u), d.grad(v))*dx
+#L = f*v*dx
+#
+#bc = d.DirichletBC(V, 0.0, d.DomainBoundary())
+#
+#u = d.Function(V)
+#
+#d.solve(a == L, u, bc)
+#print u.vector()
+#d.plot(u);d.interactive()
+
+#########
+## Next will be a little linear elastic problem:
+#########
+
+# Make a mesh for the 8x8x8 cubic-space elastic problem:
+
+mesh = d.UnitCubeMesh(8,8,8)
+#mesh = d.Cylinder(d.Point(0.,0.,0.),d.Point(0.,5.,0.),2.,6)
+#
+## Next make a function space
+V = d.VectorFunctionSpace(mesh, 'Lagrange', 1)
+
+# Create a test function and a trial function, and a source term:
+u = d.TrialFunction(V)
+w = d.TestFunction(V)
+b = d.Constant((1.0,0.,0.))
+
+# Elasticity parameters:
+E, nu = 10., 0.3
+mu, lambda_param = E / (2. * (1.+nu)), E * nu / ((1. + nu) * (1.-2. * nu))
+
+# Stress tensor:
+# + usually of form \sigma_{ij} = \lambda e_{kk}\delta_{ij} + 2\mu e_{ij},
+# + or                          = \lambda Tr(e_{ij})I + 2\mu e_{ij}, for e_{ij} the strain tensor.
+sigma = lambda_param*d.tr(d.grad(u)) * d.Identity(w.cell().d) + 2 * mu * d.sym(d.grad(u))
+
+# Governing balance equation:
+F = d.inner(sigma, d.grad(w)) * d.dx - d.dot(b,w)*d.dx
+
+# Extract the bi- and linear forms from F:
+a = d.lhs(F)
+L = d.rhs(F)
+
+# Dirichlet BC on entire boundary:
+c = d.Constant((0.,0.,0.))
+bc = d.DirichletBC(V, c, d.DomainBoundary())
+
+## Testing some new boundary definitions:
+def bzo_boundary(r_vec, on_boundary):
+
+    # NB: input r_vec is a position vector
+    r,theta,z = cart_to_cyl(x)  # NB: check the function cart_to_cyl can take non-tuple collecs.
+    return d.near(r,bzo_radius)
+
+## Testing some differing-boundary BCs:
+bzo = d.DirichletBC(V, u_bzo, bzo_boundary)
+ybco = d.DirichletBC(V, u_ybco, ybco_boundary)
+##
+
+# Finally, set up the PDE and solve it:
+u = d.Function(V)
+problem = d.LinearVariationalProblem(a, L, u, bcs=bc)
+solver = d.LinearVariationalSolver(problem)
+solver.parameters['symmetric'] = True
+s = solver.solve()
+#print u.vector()
+d.plot(u);d.interactive()
+
+#######
+## Now we'll try a more low-level (mathematically) approach
+#######
+
+#element = d.FiniteElement('Vector Lagrange', 'tetrahedron', 1)
+#
+#v = d.BasisFunction(element)
+#U = d.BasisFunction(element)
+#f = d.Function(element)
+#
+#E, nu = 10., .3
+#
+#mu = E / (2 * (1 + nu))
+#lambda_const = E * nu / ((1 + nu) * (1 - 2 * nu))
+#
+#def epsilon(v):
+#    return 0.5 * (d.grad(v) + d.transp(d.grad(v)))
+#
+#def sigma(v):
+#    return 2 * mu * epsilon(v) + lambda_const * d.mult(d.trace(epsilon(v)), d.Identity(len(v)))
+#
+#a = d.dot(d.grad(v), sigma(U)) * d.dx
+#L = d.dot(v, f) * d.dx
